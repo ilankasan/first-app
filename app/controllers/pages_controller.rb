e037aba8cd3203ea10d868a0037cb3c6abe58aa5
ilankasan <ilan.kasan1@gmail.com>
2011-01-26 22:20:08 -0800

    end chapter 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @title = "tivity Home"
   end
 
   def activities
+    @title = "tivity Activities"
   end
 
   def myteam
+    @title = "My Team"
   end
 
   def signout
+     @title = "sign out tivity"
   end
 
   def myaccount
+     @title = "tivity My Account"
   end
 
 end
---
+++ g.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -am "end chapter 3"
+git push
+cmdit push heroku
