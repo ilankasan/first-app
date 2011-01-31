865df925df0d5bdc7afc0567aa8c13a0e4930f02
ilankasan <ilan.kasan1@gmail.com>
2011-01-30 20:13:00 -0800

    added g.bat

--- g.bat
+++ g.bat
@@ -1,12 +1,20 @@
 git add .
-git commit -am "complete sign up end of chapter 8"
-#git checkout master
-#git merge signing-up
-
+git commit -m "Added user tivities - end of chapter 11"
+git checkout master
+$ git merge user-microposts
 
 git push
 git push heroku
 
 
-git push
+
+#git add .
+#git commit -am "complete sign up end of chapter 8"
+#git checkout master
+#git merge signing-up
+
+
+
+
+#git push
  
